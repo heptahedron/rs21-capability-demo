f4d3a3bc1366f05e87a4c8d211a601cf8513e489
Braxton Spence <brax.spence@gmail.com>
2016-08-24 20:03:57 -0500

    Include data, include mapbox-gl statically, include bare-bones map on page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('Hello world!')
+import mapboxgl from 'mapbox-gl'
+import './styles/mapbox-gl.css'
+
+import './styles/main.css'
+import appConfig from './app-config.json'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mapMountPoint = document.createElement('div'),
+        { mapboxAccessToken,
+          mapboxStyleUrl } = appConfig
+
+  document.body.appendChild(mapMountPoint)
+  mapboxgl.accessToken = mapboxAccessToken
+
+  let map = new mapboxgl.Map({
+    container: mapMountPoint,
+    style: mapboxStyleUrl
+  })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ const context = __dirname + '/src',
       path    = __dirname + '/dist',
       filename  = 'output.js'
 
+const mapboxShadersDir = __dirname + '/node_modules/mapbox-gl-shaders'
+
+// https://github.com/mapbox/mapbox-gl-js/blob/master/webpack.config.example.js
+
 module.exports = {
   context,
   entry,
@@ -12,14 +16,39 @@ module.exports = {
     path,
     filename
   },
+  resolve: {
+    alias: {
+      // for mapbox-gl
+      'webworkify': 'webworkify-webpack'
+    }
+  },
   module: {
     loaders: [
+      // ES6 transpilation
       { test: /\.js$/,
         loader: 'babel',
-        include: p => p.startsWith(context) }
+        include: p => p.startsWith(context) },
+      // I'll let you guess what this is for
+      { test: /\.css$/,
+        loader: 'style!css?modules' },
+      // For geospatial data and mapbox-gl
+      { test: /\.json$/,
+        loader: 'json' },
+      // for mapbox-gl's use of browserifyfs or whatever
+      { test: /\.js$/,
+        include: mapboxShadersDir + '/index.js',
+        loader: 'transform/cacheable?brfs' }
+    ],
+    postLoaders: [
+      { include: p => p.startsWith(mapboxShadersDir + '/'),
+        loader: 'transform', 
+        query: 'brfs' }
     ]
   },
   plugins: [new HtmlWebpackPlugin({
     title: 'Albuquerque Geographical Data'
-  })]
+  })],
+  // apparently can't use eval or eval-source-map, though I don't particularly
+  // want to
+  devtool: 'cheap-source-map'
 }
