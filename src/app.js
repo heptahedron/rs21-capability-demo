ba3177d220e098ec19a07b275c3179709850bc31
Braxton Spence <brax.spence@gmail.com>
2016-08-28 13:23:55 -0500

    Clean up code, temporarily disable tweets rendering, commence work on area place type composition vis

--- src/app.js
+++ src/app.js
@@ -26,104 +26,111 @@ function initApp() {
           mainMap.on('load', () => resolve(mainMap))
         })
 
-  let searchArea = turf.featureCollection(
-    [turf.buffer(turf.point(mapConfig.center), 1)])
+  awaitMapReady
+    .then(map => { addDataLayers(map); return map })
+    .then(map => { addClickInteractivity(map); return map })
+}
 
+function addDataLayers(map) {
+  map.addSource('censusData', {
+    type: 'geojson',
+    data: censusData
+  })
 
-  awaitMapReady.then(map => {
-    map.addSource('censusData', {
-      type: 'geojson',
-      data: censusData
-    })
+  map.addLayer({
+    id: 'age',
+    source: 'censusData',
+    type: 'fill',
+    paint: {
+      'fill-opacity': 0.5,
+      'fill-color': {
+        property: 'medianAge',
+        stops: [[0, '#fff'], [100, '#000']]
+      }
+    }
+  })
 
-    map.addSource('fbplaces', {
-      type: 'geojson',
-      data: fbData
-    })
+  map.addSource('fbplaces', {
+    type: 'geojson',
+    data: fbData
+  })
 
-    map.addSource('tweets', {
-      type: 'geojson',
-      data: tweetData
-    })
-
-    map.addSource('searchArea', { type: 'geojson', data: searchArea })
-    map.addLayer({
-      id: 'searchArea',
-      source: 'searchArea',
-      type: 'fill',
-      layout: {
-        'visibility': 'none'
+  map.addLayer({
+    id: 'checkinMagnitude',
+    source: 'fbplaces',
+    type: 'circle',
+    paint: {
+      'circle-radius': {
+        property: 'nCheckins',
+        stops: [
+          [0, 5],
+          [500, 10]
+        ]
       },
-      paint: {
-        'fill-color': 'rgba(0,0,255,.3)'
-      }
-    })
+      'circle-opacity': .4
+    }
+  })
 
-    map.addLayer({
-      id: 'age',
-      source: 'censusData',
-      type: 'fill',
-      paint: {
-        'fill-opacity': 0.5,
-        'fill-color': {
-          property: 'medianAge',
-          stops: [[0, '#fff'], [100, '#000']]
-        }
-      }
-    })
+  /*
+  map.addSource('tweets', {
+    type: 'geojson',
+    data: tweetData
+  })
 
-    map.addLayer({
-      id: 'checkinMagnitude',
-      source: 'fbplaces',
-      type: 'circle',
-      paint: {
-        'circle-radius': {
-          property: 'nCheckins',
-          stops: [
-            [0, 5],
-            [500, 10]
-          ]
-        },
-        'circle-opacity': .4
+  map.addLayer({
+    id: 'tweets',
+    source: 'tweets',
+    type: 'circle',
+    paint: {
+      'circle-radius': 5,
+      'circle-color': {
+        property: 'hour',
+        stops: [[0, '#000'], [12, '#ff0'], [23.99, '#000']]
       }
-    })
+    }
+  })
+  */
+}
 
-    map.addLayer({
-      id: 'tweets',
-      source: 'tweets',
-      type: 'circle',
-      paint: {
-        'circle-radius': 5,
-        'circle-color': {
-          property: 'hour',
-          stops: [[0, '#000'], [12, '#ff0'], [23.99, '#000']]
-        }
-      }
-    })
+function addClickInteractivity(map) {
+  let searchArea = turf.featureCollection(
+    [turf.buffer(turf.point([0,0]), 1)])
 
-    map.on('click', function(e) {
-      let newSearchArea = turf.featureCollection(
-            [turf.buffer(turf.point(e.lngLat.toArray()), 1)]),
-          results       = turf.collect(newSearchArea, fbData,
-                                 'typeNum', 'placeTypes'),
-          placeTypeDist = results.features[0].properties.placeTypes
-            .map(typeNum => {
-              return fbData.properties.placeTypes[typeNum].typeStr
+  map.addSource('searchArea', { type: 'geojson', data: searchArea })
+  map.addLayer({
+    id: 'searchArea',
+    source: 'searchArea',
+    type: 'fill',
+    layout: {
+      'visibility': 'none'
+    },
+    paint: {
+      'fill-color': 'rgba(0,0,255,.3)'
+    }
+  })
+
+  map.on('click', function(e) {
+    let newSearchArea = turf.featureCollection(
+      [turf.buffer(turf.point(e.lngLat.toArray()), 1)]),
+      results       = turf.collect(newSearchArea, fbData,
+                                   'typeNum', 'placeTypes'),
+      placeTypeDist = results.features[0].properties.placeTypes
+        .map(typeNum => {
+          return fbData.properties.placeTypes[typeNum].typeStr
+        })
+        .reduce((acc, typeStr) => {
+          if (typeof acc[typeStr] === 'undefined') {
+            return Object.assign(acc, {
+              [typeStr]: 1
             })
-            .reduce((acc, typeStr) => {
-              if (typeof acc[typeStr] === 'undefined') {
-                return Object.assign(acc, {
-                  [typeStr]: 1
-                })
-              }
+          }
 
-              ++acc[typeStr]
-              return acc
-            }, {})
-      console.log(placeTypeDist)
-      map.getSource('searchArea').setData(newSearchArea)
-      map.setLayoutProperty('searchArea', 'visibility', 'visible')
-    })
+          ++acc[typeStr]
+          return acc
+        }, {})
+
+        map.getSource('searchArea').setData(newSearchArea)
+        map.setLayoutProperty('searchArea', 'visibility', 'visible')
   })
 }
 
